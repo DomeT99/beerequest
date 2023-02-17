ddaf5dc7bfd108c8fe2a8889c596fab8ffb10413
Domenico <domenicotenace99@gmail.com>
2023-02-17 19:30:23 +0100

    feat: PUT, PATCH, DELETE test suite

--- tests/call.spec.ts
+++ tests/call.spec.ts
@@ -49,6 +49,79 @@ describe("Test success call", () => {
 
 		expect(result).toEqual(resultModel);
 	});
+
+	test("PUT request", async () => {
+		let resultModel: SecondResult = {
+			id: 1,
+			title: "foo",
+			body: "bar",
+			userId: 1,
+		};
+
+		fetchMock.mockResponse(JSON.stringify(resultModel));
+		let request = new BeeRequest();
+
+		let result = await request.useFetch<SecondResult>(
+			"https://jsonplaceholder.typicode.com/posts/1",
+			{
+				method: "PUT",
+				body: JSON.stringify({
+					id: 1,
+					title: "foo",
+					body: "bar",
+					userId: 1,
+				}),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			}
+		);
+
+		expect(result).toEqual(resultModel);
+	});
+
+	test("PATCH request", async () => {
+		let resultModel: SecondResult = {
+			id: 1,
+			title: "foo",
+			body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+			userId: 1,
+		};
+
+		fetchMock.mockResponse(JSON.stringify(resultModel));
+		let request = new BeeRequest();
+
+		let result = await request.useFetch<SecondResult>(
+			"https://jsonplaceholder.typicode.com/posts/1",
+			{
+				method: "PATCH",
+				body: JSON.stringify({
+					title: "foo",
+				}),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+				},
+			}
+		);
+
+		expect(result).toEqual(resultModel);
+	});
+
+	test("DELETE request", async () => {
+		let resultModel = {};
+
+		fetchMock.mockResponse(JSON.stringify(resultModel));
+		let request = new BeeRequest();
+
+		let result = await request.useFetch<SecondResult>(
+			"https://jsonplaceholder.typicode.com/posts/1",
+			{
+				method: "DELETE",
+			}
+		);
+
+		expect(result).toEqual(resultModel);
+	});
 });
 
 interface FirstResult {
